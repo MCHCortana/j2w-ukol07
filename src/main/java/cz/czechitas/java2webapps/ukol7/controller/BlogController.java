c9bdcd706a86dc1c3eaea906ebc8647f0a9cdb5c
MCHCortana <wolokika@gmail.com>
2024-07-12 17:08:51 +0200

    fixed - file refractoring freemarker_implicit.ftl to recources folder

--- src/main/java/cz/czechitas/java2webapps/ukol7/controller/BlogController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/BlogController.java
@@ -2,11 +2,14 @@ package cz.czechitas.java2webapps.ukol7.controller;
 
 import cz.czechitas.java2webapps.ukol7.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.awt.print.Pageable;
 
 @Controller
 @RequestMapping("/")
@@ -25,7 +28,7 @@ public class BlogController {
     }
 
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView seznamBlogPostu() {
         return new ModelAndView("index")
                 .addObject("seznamBlogovychPostu", blogService.blogList());
     }
